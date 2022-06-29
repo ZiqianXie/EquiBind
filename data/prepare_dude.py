95816c87a093d718c35e5b2654be57acbb9c48dd
Xie <zxie3@sbbindplp004.uthouston.edu>
2022-06-29 12:58:56 -0500

    save keypoint features

--- data/prepare_dude.py
+++ data/prepare_dude.py
@@ -8,8 +8,8 @@ import os
 dude_target_names = set(map(lambda x: os.path.split(x)[-1], glob("all/*")))
 table = pd.read_html("http://dude.docking.org/targets")[1]
 name_to_pdb_mapping = dict(zip(table['Target Name'].str.lower(), table.PDB))
-pdbbind_processed = set(map(lambda x: os.path.split(x)[-1], glob("PDBBind_processed")))
-dude_pbd = set(map(lambda x: name_to_pdb_mapping[x], dude_target_names))
+pdbbind_processed = set(map(lambda x: os.path.split(x)[-1], glob("PDBBind_processed/*")))
+dude_pdb = set(map(lambda x: name_to_pdb_mapping[x], dude_target_names))
 overlapped = dude_pdb & pdbbind_processed
 with open('dude', 'w') as f:
     f.write('\n'.join(overlapped))
