0d66b157b2df4f37d7fd222b4f84f47b68f44490
ZiqianXie <z.xie4@umiami.edu>
2022-07-20 10:06:15 -0500

    fix small bug

--- multiligand_inference.py
+++ multiligand_inference.py
@@ -132,7 +132,7 @@ def run_batch(model, ligs, lig_coords, lig_graphs, rec_graphs, geometry_graphs, 
     try:
         output = model(lig_graphs, rec_graphs, geometry_graphs, output_keypoint_representation=True)
         predictions = output[0]
-        rec_feat_keypts = output[-2].cpu().numpy()
+        rec_feat_keypts = torch.cat(output[-2]).cpu().numpy()
         lig_feat_keypts = torch.cat(output[-1]).cpu().numpy()
         out_ligs = ligs
         out_lig_coords = lig_coords
